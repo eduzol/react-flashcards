0f196c66a49cf3096e76b16f303e4409f716b6b8
EduardoZola <eduzol@gmail.com>
2017-11-24 00:25:19 -0800

    DeckList initial styling

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import { FlatList, TouchableOpacity,Text,  View } from 'react-native';
+import {StyleSheet, FlatList, TouchableOpacity,Text,  View } from 'react-native';
 
 class DeckList extends Component {
 
     render(){
         return (
-            <View>
+            <View style={styles.container} >
                 <Text>This is a Deck List view</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
 
 export default DeckList;
