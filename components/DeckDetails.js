d78a0080ec2112ee3579000df4c09868ab69b3ea
EduardoZola <eduzol@gmail.com>
2017-12-02 20:21:49 -0800

    Connect DeckDetails to redux middleware

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import {StyleSheet, FlatList, TouchableOpacity,Text,  View } from 'react-native';
+import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 import * as  DeckAPI from '../util/api.js'; 
 import Deck from './Deck';
 
 class DeckDetails extends Component {
 
-    state = {
-        deck : {}
-    };
-
-    componentDidMount(){
-       
-        let deck = this.props.navigation.state.params.deck;
-        this.setState({deck});
-
-    }
-    
     render () {
-        let item = this.state.deck;
-       
+        let item = this.props.deck;
+    
         return (
             <View style={styles.container} >
                
@@ -55,5 +46,22 @@ const styles = StyleSheet.create({
 });
   
 
-export default DeckDetails;
+function mapStateToProps (state, { navigation }) {
+    let deckId = navigation.state.params.deck.id;
+   
+    let deck = state.decks.find( (deck) => deck.id === deckId);
+    deck.cards = state.cards.filter((card) => card.deckId === deckId );
+   
+    return  {
+            deck 
+    };
 
+}
+  
+function mapDispatchToProps(dispatch, { navigation }){
+    return {
+        navigate: data => navigation.dispatch(NavigationActions.navigate(data)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeckDetails);
